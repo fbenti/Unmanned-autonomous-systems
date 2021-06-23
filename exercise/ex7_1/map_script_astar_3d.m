3784c462a0062efaf2c683764f912c31703bc13e
DavidWuthier <dwuthier@gmail.com>
2021-06-23 15:13:21 +0200

    7_1 change

--- exercise/ex7_1/map_script_astar_3d.m
+++ exercise/ex7_1/map_script_astar_3d.m
@@ -1,6 +1,9 @@
 close all
 clear all
 clc
+% Load parameters
+load('uas_parameters.mat');
+
 %% Setup the map
 % Load the map from the wall file. Each line, except the two last, in the
 % wall file is an obstacle. The position of the obstacle is defined in the
@@ -9,7 +12,7 @@ clc
 % load('wall_test.txt')
 % If a different named file is used, then write it into the wall variable
 % e.g. wall = maze_2;
-wall = load('auto_wall.txt');
+wall = load('wall.txt');
 
 % Define the map size
 max_x = wall(length(wall) - 2, 1);
@@ -28,63 +31,71 @@ end
 % Define the starting and end position
 % start = wall(length(wall) - 1, :);
 % end_ = wall(length(wall), :);
-start = [0 0 1] + [1 1 0];
-end_=[4 5 1];
+E = [1 1 1];
+F = [2 1 1];
+G = [3 1 1];
+H = [4 1 1];
+
+A = [1 5 1];
+B = [2 5 1];
+C = [3 5 1];
+D = [4 5 1];
+start = H;
+end_ = D;
 
 % Make sure the start and end is not an obstacle
 map(start(1), start(2), start(3)) = 0;
 map(end_(1), end_(2), end_(3)) = 0;
 
-
 %% Run the algorithm to optain the route
 route = greedy_astar_3d(map, start, end_, 1);
 
-% route = [route; 4 5 0];
+route = [route; end_ - [0 0 1]];
 
 % route = optimized_route(route)
-%% Draw the map
-% Draw a figure to show the map and process
-hold off
-figure(1)
-% Mark the start with green
-scatter3(start(1)+0.5, start(2)+0.5, start(3)+0.5, ...
-         500, [0,1,0],'filled')
-hold on
-
-% Mark the end with red
-scatter3(end_(1)+0.5, end_(2)+0.5, end_(3)+0.5, ...
-         500, [1,0,0], 'filled')
-hold on
-
-% Draw the obstacles
-for i = 1:(length(wall) - 3)
-    map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
-                        wall(i, 2) wall(i, 2) + 1;...
-                        wall(i, 3) wall(i, 3) + 1], map, 1);
-    
-end
-
-% Set the axes
-axis([1 max_x+1 1 max_y+1 1 max_z+1])
-% Make the grid lines more visible
-ax = gca;
-ax.GridAlpha = 1.0;
-grid on
-set(gca, 'xtick', [0:1:max_x])
-set(gca, 'ytick', [0:1:max_y])
-set(gca, 'ztick', [0:1:max_z])
-%% Draw the route
-pause on;
-for i = 2:length(route)
-    plot3([route(i-1,1)+0.5,route(i,1)+0.5], ...
-          [route(i-1,2)+0.5,route(i,2)+0.5], ...
-          [route(i-1,3)+0.5,route(i,3)+0.5], ...
-          'color',[0,0,0],'linewidth',5)
-    hold on
-    pause(0.1)
-    route(i,:)
-end
-hold off
+% %% Draw the map
+% % Draw a figure to show the map and process
+% hold off
+% figure(1)
+% % Mark the start with green
+% scatter3(start(1)+0.5, start(2)+0.5, start(3)+0.5, ...
+%          500, [0,1,0],'filled')
+% hold on
+% 
+% % Mark the end with red
+% scatter3(end_(1)+0.5, end_(2)+0.5, end_(3)+0.5, ...
+%          500, [1,0,0], 'filled')
+% hold on
+% 
+% % Draw the obstacles
+% for i = 1:(length(wall) - 3)
+%     map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
+%                         wall(i, 2) wall(i, 2) + 1;...
+%                         wall(i, 3) wall(i, 3) + 1], map, 1);
+%     
+% end
+% 
+% % Set the axes
+% axis([1 max_x+1 1 max_y+1 1 max_z+1])
+% % Make the grid lines more visible
+% ax = gca;
+% ax.GridAlpha = 1.0;
+% grid on
+% set(gca, 'xtick', [0:1:max_x])
+% set(gca, 'ytick', [0:1:max_y])
+% set(gca, 'ztick', [0:1:max_z])
+% %% Draw the route
+% pause on;
+% for i = 2:length(route)
+%     plot3([route(i-1,1)+0.5,route(i,1)+0.5], ...
+%           [route(i-1,2)+0.5,route(i,2)+0.5], ...
+%           [route(i-1,3)+0.5,route(i,3)+0.5], ...
+%           'color',[0,0,0],'linewidth',5)
+%     hold on
+%     pause(0.1)
+%     route(i,:)
+% end
+% hold off
 
 %% Scale the route
 x_scale = 0.65;
@@ -93,7 +104,7 @@ z_scale = 0.75;
 
 x_offset = 0.3;
 y_offset = 0.5;
-z_offset = 0.25;
+z_offset = 0.55;
 
 % Make a copy of the route
 route_scaled = route;
@@ -103,8 +114,15 @@ route_scaled(:,1) = (route_scaled(:,1) - 1) * x_scale + x_offset;
 route_scaled(:,2) = (route_scaled(:,2) - 1) * y_scale + y_offset;
 route_scaled(:,3) = (route_scaled(:,3) - 1) * z_scale + z_offset;
 
+for i= 1:length(route_scaled)
+    if route_scaled(i,3) >= 1.75
+        route_scaled(i,3) = 1.75;
+    end
+end
+
 % Print the scaled route
-route_scaled = [route_scaled; 2.3 2.7 0.25; 2.3 2.6 0.25; 2.3 2.6 0]
-route_scaled
-length(route)
+% route_scaled = [route_scaled; 2.3 2.7 0.25; 2.3 2.6 0.25; 2.3 2.6 0]
+route
+route = route_scaled
+
 
